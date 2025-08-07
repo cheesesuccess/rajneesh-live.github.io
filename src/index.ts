42d9f9302ae1c239d1ad88662a222d81f2f6c3a7
cheesesuccess <natashasibanda2025@outlook.com>
2025-08-07 21:51:40 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,29 @@
-import { Router } from 'solid-app-router'
-import { createApp } from 'solid-utils'
-import { MusicImagesProvider } from './components/music-image/data-context'
-import { MenuProvider } from './components/menu/menu'
-import { RootStoresProvider } from './stores/stores'
-import { ModalsProvider } from './components/modals/modals'
-import { ErrorPage } from './pages/error/error'
-import { App } from './pages/app/app'
-
-// The supported browser features check file is very small,
-// still in case if it doesn't load or loads late
-// do not render app only if we explicitly know that browser is not supported.
-// If app loads in unsupported browser because of race condition it's not a big deal.
-
-import { onAuthStateChanged } from "firebase/auth";
+import { createApp } from 'solid-utils';
+import { Router } from 'solid-app-router';
+import { App } from './pages/app/app';
+import { ErrorPage } from './pages/error/error';
+import { MusicImagesProvider } from './components/music-image/data-context';
+import { MenuProvider } from './components/menu/menu';
+import { RootStoresProvider } from './stores/stores';
+import { ModalsProvider } from './components/modals/modals';
 import { auth } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 
+// Automatically redirect unauthenticated users to /auth,
+// but avoid infinite loop by skipping if already on /auth
 onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, load the app
-    if (window.isSupportedBrowser !== false) {
-      createApp(App)
-        .use(Router)
-        .use(ErrorPage)
-        .use(MusicImagesProvider)
-        .use(RootStoresProvider)
-        .use(ModalsProvider)
-        .use(MenuProvider)
-        .mount('body');
-    }
+  const currentPath = window.location.pathname;
+
+  if (user || currentPath.startsWith('/auth')) {
+    createApp(App)
+      .use(Router)
+      .use(ErrorPage)
+      .use(MusicImagesProvider)
+      .use(RootStoresProvider)
+      .use(ModalsProvider)
+      .use(MenuProvider)
+      .mount('body');
   } else {
-    // Not signed in, redirect to auth page
     window.location.href = "/auth";
   }
 });
